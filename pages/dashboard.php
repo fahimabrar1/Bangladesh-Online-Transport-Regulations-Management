2d68e53f384239b24f39eea4025071ad408748d7
Fahim Abrar <fahim.abrar101@gmail.com>
2020-12-14 00:57:59 +0600

    botrm-dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -7,13 +7,96 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    
+    
+    <link href="/css/style.css" rel="stylesheet">
+    <title>Bangladesh Online Transport Regulations And Management</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  
+  
+<!--Nav Bar -->
 
-    <!-- Optional JavaScript -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">BOTRM</a>
+      
+      
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+   <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+   
+       <a class="nav-item nav-link active" href="#">Dashboard<span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">About Us</a>
+      <a class="nav-item nav-link" href="#">Contact</a>
+      <a class="nav-item nav-link" href="#">Login</a>
+    
+    </div>
+  </div>
+  
+</nav>
+   
+   
+   
+
+<div class="d-flex justify-content-center">
+   <div class="container mt-5">
+    <!--Search Bar -->
+    <form class="form-inline mb-1">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form> 
+        
+
+  
+<!--Search Result Table Bar -->
+
+<div class="container p-3 my-3 border scrollable">
+ 
+  <table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Driver</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+      <th scope="col" class="d-flex justify-content-around">Go To Profile</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td colspan="3" >Mark</td>
+      <td class="d-flex justify-content-around">
+          <button type="button" class="btn btn-danger">Go To Profile</button></td>
+    </tr>
+
+    <tr>
+      <th scope="row">2</th>
+      <td  colspan="3"  >Jacob</td>
+      <td class="d-flex justify-content-around">
+          <button type="button" class="btn btn-danger">Go To Profile</button></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="3" >Larry</td>
+      <td class="d-flex justify-content-around">
+          <button type="button" class="btn btn-danger">Go To Profile</button></td>
+    </tr>
+    
+    
+  </tbody>
+  </table>
+
+</div>
+  </div>
+</div>
+                       
+                          
+                                
+                        <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
