fc6a9f2adc813d5944f97676e7ac4cce66b8ec4e
Fahim Abrar <fahim.abrar101@gmail.com>
2020-12-16 16:43:20 +0600

    fahim-botrm-3
    Added Driver edit button and Vehicle Info and File Case Button

--- pages/driverprofile.php
+++ pages/driverprofile.php
@@ -75,14 +75,44 @@
             <div class="col">
                 <div class="container-fluid">
                   <div class=" col-sm-11 d-flex justify-content-end">
-                        <a type="button" class="btn btn-danger text-white col-6" >File A Case</a>
+                        <a type="button" class="btn btn-danger text-white " >Edit Profile</a>
                   </div>
                </div>
             </div>
        </div>
    </div>
-        
 
+<div class="container p-3 mt-2 d-flex justify-content-center"><h1>VEHICLES</h1></div>                
+
+<div class="container p-3 mt-2 border rounded">
+       <div class="row">
+             <div class="col">
+                <div class="container-fluid">
+                  <div class=" col-sm-12 d-flex justify-content-end">
+                        <a type="button" class="btn btn-danger text-white col-3 p-2 mb-2" >File A Case</a>
+                  </div>
+                </div>   
+             </div>
+            
+               <div class="container-fluid row">
+                   <div class="col-sm-6">
+                       <h5>Vehicle Type : </h5>
+                   </div>
+                   <div class="col">
+                       <h5>Motor-Bike</h5>
+                   </div>
+               </div>
+               <div class="container-fluid row">
+                   <div class="col-sm-6">
+                       <h5>Vehicle Number : </h5>
+                   </div>
+                   <div class="col">
+                       <h5>HA-12-12-12</h5>
+                   </div>
+               </div>
+               
+        </div>
+</div>
 
                        
                           
